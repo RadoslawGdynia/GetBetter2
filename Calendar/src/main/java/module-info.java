6480cf167f8035e288a6df9cb82b04af45907d20
RadoslawGdynia <72702353+RadoslawGdynia@users.noreply.github.com>
2020-12-02 18:16:27 +0100

    On the way of naming improvements

---
+++ Calendar/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+open module Calendar {
+
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires slf4j.api;
+
+    exports Calendar;
+    exports Datasources;
+    exports Day;
+    exports Controllers;
+    exports Task;
+    exports Tiles;
+
+
+
+}
---
+++ MainWindow/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.RadoslawGdynia.GetBetter.MainWindow {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    requires Calendar;
+    requires slf4j.api;
+
+    opens Main;
+}
